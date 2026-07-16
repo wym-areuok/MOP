058dc8cc06b4ff6dff9e9975521371d8c82c5775
weiyiming <997990179@email.com>
2026-07-16 17:00:18 +0700

    补充代码提交

--- mop-common/src/main/java/com/mop/common/core/domain/model/LoginBody.java
+++ mop-common/src/main/java/com/mop/common/core/domain/model/LoginBody.java
@@ -1,29 +1,36 @@
 package com.mop.common.core.domain.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * 用户登录对象
  *
  * @author weiyiming
  */
+@Schema(description = "登录请求体")
 public class LoginBody {
     /**
      * 用户名
      */
+    @Schema(description = "用户名", required = true, example = "admin")
     private String username;
 
     /**
      * 用户密码
      */
+    @Schema(description = "用户密码", required = true, example = "admin123")
     private String password;
 
     /**
      * 验证码
      */
+    @Schema(description = "验证码", example = "1234")
     private String code;
 
     /**
      * 唯一标识
      */
+    @Schema(description = "验证码唯一标识", example = "abcd1234")
     private String uuid;
 
     public String getUsername() {
